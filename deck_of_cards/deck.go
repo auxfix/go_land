c4fde41a9598db48e811f212a22d2c43ebd1dd7e
oleg <music.wave.form7@gmail.com>
2022-02-02 07:52:04 +0100

    my first reciever

--- deck_of_cards/deck.go
+++ deck_of_cards/deck.go
@@ -1,3 +1,24 @@
 package main
 
+import "fmt"
+
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Dimonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for i, suit := range cardSuits {
+		for j, value := range cardSuits{
+			cards = append(cards, suit" of "value)
+		}
+	}
+}
+
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- deck_of_cards/main.go
+++ deck_of_cards/main.go
@@ -1,20 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
-	cards := deck{newCard(1), newCard(2), newCard(3)}
+	cards := deck{"two", "three", "four"}
 
-	cards = append(cards, newCard(6))
+	cards = append(cards, "One")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard(ndx int) string {
-	return "__Five of Diamonds(" + strconv.Itoa(ndx) + ")  "
+	cards.print()
 }
